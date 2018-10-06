a8a9224f01258276e7050980f114ef3d1e7ab879
Curtis Brazzell <curtbraz@gmail.com>
2018-10-05 23:47:07 -0400

    Update templatecreation.php

--- templates/templatecreation.php
+++ templates/templatecreation.php
@@ -61,7 +61,7 @@ exec($cmdzipup);
 <HTML>
 <HEAD>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="../main.css">
 </HEAD>
 <BODY><br><br><br>
 <FORM ACTION="<?php echo $templatename; ?>.zip" METHOD="GET">
